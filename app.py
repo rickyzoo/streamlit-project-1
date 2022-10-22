073cbd596094125d358d582d94ae442b3e921f03
Ricky Zou <ricky.youngz35@gmail.com>
2022-10-21 21:32:08 -0400

    import sklearn

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
